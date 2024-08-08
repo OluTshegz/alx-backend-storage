990049f73219c06cc615c067577929cacc0840a9
Olusegun <ayosegun2001@gmail.com>
2024-08-09 00:35:25 +0100

    0x00. MySQL advanced: Back-end, SQL, MYSQL

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -2,7 +2,7 @@
 -- Task: Create an index on the table 'names' based on the first letter of 'name'.
 
 -- Ensure that the index does not already exist
-DROP INDEX IF EXISTS idx_name_first ON names;
+-- DROP INDEX IF EXISTS idx_name_first ON names;
 
 -- Create an index named 'idx_name_first' on the 'names' table.
 -- This index will be created on the first letter of the 'name' column.
--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -2,7 +2,7 @@
 -- Task: Create an index on the table 'names' for the first letter of 'name' and the 'score'.
 
 -- Ensure that the index does not already exist
-DROP INDEX IF EXISTS idx_name_first_score ON names;
+-- DROP INDEX IF EXISTS idx_name_first_score ON names;
 
 -- Create an index named 'idx_name_first_score' on the 'names' table.
 -- This index will be created on the first letter of the 'name' column and the 'score' column.
