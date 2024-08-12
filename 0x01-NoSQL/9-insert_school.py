b4ff22b07c5b7186f37653e7dd55fe77445a2722
Olusegun <ayosegun2001@gmail.com>
2024-08-13 00:41:13 +0100

    0x01. NoSQL: Back-end, NoSQL, MongoDB

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Insert a new document into a MongoDB
+collection based on keyword arguments.
+
+Prototype: def insert_school(mongo_collection, **kwargs):
+- mongo_collection will be the pymongo collection object
+- Returns the new _id of the inserted document
+"""
+
+from pymongo.collection import Collection
+from typing import Any
+
+
+def insert_school(mongo_collection: Collection, **kwargs: Any) -> str:
+    """
+    Inserts a new document into the given MongoDB
+    collection based on the provided keyword arguments.
+
+    Parameters:
+    mongo_collection (Collection): The pymongo collection object
+                                    into which the document will be inserted.
+    **kwargs (Any): Keyword arguments representing the fields
+                    and values of the document to be inserted.
+
+    Returns:
+    str: The _id of the newly inserted document.
+    """
+    # Insert the document into the collection
+    # with the provided keyword arguments
+    result = mongo_collection.insert_one(kwargs)
+
+    # Return the _id of the newly inserted document
+    return str(result.inserted_id)
